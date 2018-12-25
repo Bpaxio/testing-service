1ad6e21861965362596b4c13f4d26a1aece32c35
Владислав Рахлинский <BBPax@yandex.ru>
2018-12-25 23:47:52 +0300

    migrated project to Spring Boot

--- src/main/java/ru/otus/bbpax/config/AppConfiguration.java
+++ src/main/java/ru/otus/bbpax/config/AppConfiguration.java
@@ -1,16 +1,17 @@
 package ru.otus.bbpax.config;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import ru.otus.bbpax.service.ActionRunner;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
 @Configuration
-@PropertySource({"classpath:config.properties"})
 public class AppConfiguration {
 
     @Bean
@@ -27,8 +28,17 @@ public class AppConfiguration {
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource ms
             = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("/msg");
+        ms.setBasename("classpath:msg");
         ms.setDefaultEncoding("UTF-8");
         return ms;
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            ActionRunner exam = ctx.getBean(ActionRunner.class);
+            exam.setUp();
+            exam.run();
+        };
     }
 }
